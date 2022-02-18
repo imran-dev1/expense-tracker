9475e470693ab100388844b21c5c0cd9202bce32
webdev-it <webdevit3@gmail.com>
2022-02-18 21:22:03 +0600

    Error handled

--- js/app.js
+++ js/app.js
@@ -2,51 +2,87 @@ function getValue(id) {
    const field = document.getElementById(id);
    return field;
 }
+const income = getValue('income-field');
+const food = getValue('food-field');
+const rent = getValue('rent-field');
+const clothes = getValue('clothes-field');
+
+// Error Handeling
+const incomeError = getValue("income-error");
+const expenseError = getValue("expense-error");
+const saveError = getValue("save-error");
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
-   const income = getValue('income-field');
-   const incomeValue = parseFloat(income.value);
-   const food = getValue('food-field');
-   const rent = getValue('rent-field');
-   const clothes = getValue('clothes-field');
+   if (isNaN(income.value) || income.value < 1) {
+      expenseError.innerText = '';
+      incomeError.innerText = "Error: Please add a valid income amount.";
+   } else if (
+      isNaN(food.value) ||
+      food.value < 0 ||
+      food.value == "" ||
+      isNaN(rent.value) ||
+      rent.value < 0 ||
+      rent.value == "" ||
+      isNaN(clothes.value) ||
+      clothes.value < 0 ||
+      clothes.value == ""
+   ) {
+      incomeError.innerText = '';
+      expenseError.innerText = "Error: Please add a valid expense amount.";
+   } else {
+      incomeError.innerText = "";
+      expenseError.innerText = "";
 
-   /* -------Total Expenses------ */
-   const newTotalExpense = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(clothes.value);
-   const previousExpense = getValue('total-expenses');
-   const updateExpense = parseFloat(previousExpense.innerText) + newTotalExpense;
-   previousExpense.innerText = updateExpense;
-   income.value = '';
-   food.value = '';
-   rent.value = '';
-   clothes.value = '';
+      /* -------Total Expenses------ */
+      const newTotalExpense = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(clothes.value);
+      if (newTotalExpense > income.value) {
+         expenseError.innerText = 'Error: Expenses are more than your income.'
+      } else {
+         const previousExpense = getValue('total-expenses');
+         const updateExpense = parseFloat(previousExpense.innerText) + newTotalExpense;
+         previousExpense.innerText = updateExpense;
+         food.value = '';
+         rent.value = '';
+         clothes.value = '';
 
-   /* -------Total Balance------ */
-   const previousBalance = getValue('total-balance');
-   const newBalance = incomeValue - newTotalExpense;
-   const updateBalanceTotal = parseFloat(previousBalance.innerText) + newBalance;
-   previousBalance.innerText = updateBalanceTotal;
+         /* -------Total Balance------ */
+         const previousBalance = getValue('total-balance');
+         const newBalance = parseFloat(income.value) - newTotalExpense;
+         const updateBalanceTotal = parseFloat(previousBalance.innerText) + newBalance;
+         previousBalance.innerText = updateBalanceTotal;
+      }
+      
 
-   /* -------Saving Handler------ */
-   
-   getValue('save-btn').addEventListener('click', function () {
-
-      /* -------Saving Amount------ */
-         const saveField = getValue('save-field');
-         const savingAmount = incomeValue * parseFloat(saveField.value) / 100;
-         
-         const previousSavingAmount = getValue('saving-amount');
-         const updateSavingAmount = parseFloat(previousSavingAmount.innerText) + savingAmount;
-   
-         previousSavingAmount.innerText = updateSavingAmount;
-         saveField.value = '';
-   
-         /* -------Remaining Balance------ */
-         const remainingField = getValue('remaining-balance');
-         remainingField.innerText = updateBalanceTotal - updateSavingAmount;
-   
-   
-      })
-
-
+   }
 
 })
+/* -------Saving Handler------ */
+const totalBalance = getValue('total-balance');
+
+document.getElementById('save-btn').addEventListener('click', function () {
+   
+   /* -------Saving Amount------ */
+   const saveField = getValue('save-field');
+   const saveFieldValue = saveField.value;
+   saveField.value = '';
+   if (isNaN(saveFieldValue) || saveFieldValue < 0) {
+      saveError.innerText = "Error: Please add a valid number";
+   } else {
+      const newSavingAmount = (income.value * saveFieldValue) / 100;
+      if (newSavingAmount > totalBalance.innerText) {
+         saveError.innerText = 'Error: You do not have enough amount to save.';
+      } else {
+         saveError.innerText = '';
+         const savingAmountField = getValue('saving-amount');
+         savingAmountField.innerText = parseFloat(savingAmountField.innerText) + newSavingAmount;
+
+         /* -------Remaining Balance------ */
+         const remainingBalance = getValue('remaining-balance');
+         remainingBalance.innerText = totalBalance.innerText - savingAmountField.innerText;
+   
+         income.value = '';
+      }
+   
+   }
+   
+});
